715f15979de26a6a5ca51d5e804c6a6496fd234e
Rose Francis <rosennabs@gmail.com>
2024-04-18 20:57:39 -0600

    Use Link to navigate routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,27 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-1/6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-black lg:p-4 lg:dark:bg-zinc-800/30 text-white">
+
+        <Link href='/'> <p className="fixed left-0 top-0 flex w-1/6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-black lg:p-4 lg:dark:bg-zinc-800/30 text-white">
           Homepage
-        </p>
+        </p> </Link>
+        
 
-        <p className="fixed left-0 top-0 flex w-1/6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-black lg:p-4 lg:dark:bg-zinc-800/30 text-white">
+        <Link href='/knowledge_products'> <p className="fixed left-0 top-0 flex w-1/6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-black lg:p-4 lg:dark:bg-zinc-800/30 text-white">
           Knowledge Products
-        </p>
+        </p> </Link>
 
-        <p className="fixed left-0 top-0 flex w-1/6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-black lg:p-4 lg:dark:bg-zinc-800/30 text-white">
+        <Link href='/relationships'> <p className="fixed left-0 top-0 flex w-1/6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-black lg:p-4 lg:dark:bg-zinc-800/30 text-white">
           Relationship building
-        </p>
+        </p> </Link>
 
-        <p className="fixed left-0 top-0 flex w-1/6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-black lg:p-4 lg:dark:bg-zinc-800/30 text-white">
+       <Link href='/clientele'> <p className="fixed left-0 top-0 flex w-1/6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-black lg:p-4 lg:dark:bg-zinc-800/30 text-white">
           Clientele
-        </p>
+        </p> </Link>
         
       </div>
 
