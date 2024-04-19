52b1b6d48968bca8663054f8c653680611640712
Rose Francis <rosennabs@gmail.com>
2024-04-18 22:12:16 -0600

    Install and setup formik for forms

--- src/app/knowledge_products/page.tsx
+++ src/app/knowledge_products/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function Knowledge_Products() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen' >
       <h1 className='text-5xl'>Knowledge Products</h1>
+      <Formik
+        initialValues={{
+          
+        }}
+        onSubmit={()=>{
+          
+        }}
+      >
+
+      </Formik>
     </div>
     
   );
