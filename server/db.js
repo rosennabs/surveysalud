4df9eae067c7862e7c30c6b479c3ad99a88a283a
Rose Francis <rosennabs@gmail.com>
2024-05-18 23:52:40 -0600

    Manage db using pgadmin4 and successfully connect to db

---
+++ server/db.js
@@ -0,0 +1,24 @@
+require("dotenv").config(); //run npm install dotenv
+
+const { Pool } = require("pg"); //run npm install pg
+
+const db = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+db.query("SELECT * FROM programs", (err, res) => {
+  console.log(err ? err.message : res.rows);
+});
+
+db.query("SELECT current_database()", (err, res) => {
+  if (err) {
+    console.error("Error executing query:", err.message);
+  } else {
+    console.log("Connected to database:", res.rows[0].current_database);
+  }
+});
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 //const db = require('./db/connection')
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
