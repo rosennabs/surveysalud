b8b982f096f1cb53bff991743036519dc63648b8
Rose Francis <rosennabs@gmail.com>
2024-07-03 01:00:20 -0600

    Build and style OurServices section

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -2,9 +2,33 @@ import React from 'react';
 
 function Services() {
   return (
-    <div>
+    <div className='flex flex-col'>
      
-        <h1>Our Services</h1>
+      <h1 className='flex justify-center uppercase'>Our Services</h1>
+      <div className='flex justify-between mt-24'>
+
+        <div className='flex flex-col border p-4 w-80'>
+          <img className="rounded-xl" src="https://picsum.photos/id/20/300" alt="image placeholder" />
+          <h4 className='flex justify-center mt-8 mb-4'>Data Analysis</h4>
+          <p className='flex text-xs justify-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum quam vitae massa fermentum tempor. Curabitur pretium velit quis imperdiet.</p>
+          <span className='flex text-xs text-teal-500 underline underline-offset-2 hover:text-gray-400 hover:cursor-pointer justify-center mt-8'>Read more...</span>
+        </div>
+
+        <div className='flex flex-col border p-4 w-80'>
+          <img className="rounded-xl" src="https://picsum.photos/id/20/300" alt="image placeholder" />
+          <h4 className='flex justify-center mt-8 mb-4'>Reports</h4>
+          <p className='flex text-xs justify-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum quam vitae massa fermentum tempor. Curabitur pretium velit quis imperdiet.</p>
+          <span className='flex text-xs text-teal-500 underline underline-offset-2 hover:text-gray-400 hover:cursor-pointer justify-center mt-8'>Read more...</span>
+        </div>
+
+        <div className='flex flex-col border p-4 w-80'>
+          <img className="rounded-xl" src="https://picsum.photos/id/20/300" alt="image placeholder" />
+          <h4 className='flex justify-center mt-8 mb-4'>PMF</h4>
+          <p className='flex text-xs justify-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum quam vitae massa fermentum tempor. Curabitur pretium velit quis imperdiet.</p>
+          <span className='flex text-xs text-teal-500 underline underline-offset-2 hover:text-gray-400 hover:cursor-pointer justify-center mt-8'>Read more...</span>
+        </div>
+        
+      </div>
      
     </div>
   );
--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Team() {
   return (
-    <div>
-      <h1>Meet Our Team</h1>
+    <div className='flex flex-col'>
+      <h1 className='flex justify-center uppercase'>Meet Our Team</h1>
+      <div className='my-24 p-2 border '>
+        <img className="rounded-xl" src="https://picsum.photos/id/65/300/400" alt="image placeholder" />
+      </div>
+      <div className='p-2 border'>
+        <img className="rounded-xl" src="https://picsum.photos/id/65/300/400" alt="image placeholder" />
+      </div>
     </div>
   );
 }
